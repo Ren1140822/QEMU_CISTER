171b1bfe50d2e285c98a7bb10b95fe91bdcd57ee
Renato Oliveira <1140822@isep.ipp.pt>
2017-09-14 17:54:07 +0100

    added GITIGNORE

--- Src/virtual_machine_actions.c
+++ Src/virtual_machine_actions.c
@@ -5,6 +5,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+
+/**
+    Starts a i386 virtual machine receiving a disk name and iso name as params.
+**/
 void start_i386(char * disk_name, char *iso_name)
 {
     char * exec_cmd = (char *) malloc(strlen(I386_START)+strlen(disk_name)+strlen(iso_name)+ sizeof(char));
@@ -12,7 +16,7 @@ void start_i386(char * disk_name, char *iso_name)
     sprintf(exec_cmd, I386_START, disk_name, iso_name);
 
     pid_t pid =fork();
-    
+
     if(pid==0)
     {
         system(exec_cmd);
